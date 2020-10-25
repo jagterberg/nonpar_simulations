cc261bbfac789dd237eeed3a66131575ea8d3d6c
Joshua <jagterb1@jhu.edu>
2020-10-25 15:44:10 -0400

    rerunning for smaller n

--- runall.R
+++ runall.R
@@ -32,52 +32,57 @@ if(!require(Matrix)) {
 # numcores <- detectCores()
 # registerDoParallel(cores=3)
 epsilons <- c(0,.1,.2)
-ns <- c(300,500,700)#600,900)
+ns <- c(100,200,300,400,500)#,500,700)#600,900)
 cores=detectCores()
 cl <- makeCluster(cores[1]-1) #not to overload your computer
-registerDoParallel(cl,outfile = "sbm_sim_300.txt")
+registerDoParallel(cl)
 
 
 #cl <- makeCluster() #not to overload your computer
 #registerDoParallel(cl)
 print(paste0("packages loaded, running SBM simulation"))
 
+results <- foreach(n = ns,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
+        ,.noexport = "generateAdjacencyMatrix" )  %:% 
 
-results_sbm_300 <- foreach(eps=epsilons,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
-         ,.noexport = "generateAdjacencyMatrix" )  %dopar% {
-           source("./balanced_sbm/sbm_hyp_test.R")
-           #print(paste("eps = ",eps,", n = ",n))
-           run_simulation_sbm(eps = eps,ntimes = 500,n=ns[1],nMC = 500)
-  }
+            foreach(eps=epsilons,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
+                                     ,.noexport = "generateAdjacencyMatrix" )  %dopar% {
+                                       source("./balanced_sbm/sbm_hyp_test.R")
+                                       #print(paste("eps = ",eps,", n = ",n))
+                                       run_simulation_sbm(eps = eps,ntimes = 100,n=n,nMC = 500)
+        }
+          
+        
+
 
 stopCluster(cl)
-save(results_sbm_300,file = "sbm_results_10-21_300.Rdata")
-print(paste("finished n =",ns[1]))
-cl <- makeCluster(cores[1]-1) #not to overload your computer
-registerDoParallel(cl,outfile = "sbm_sim_500.txt")
-results_sbm_500 <- foreach(eps=epsilons,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
-                            ,.noexport = "generateAdjacencyMatrix" )  %dopar% {
-                              source("./balanced_sbm/sbm_hyp_test.R")
-                              #print(paste("eps = ",eps,", n = ",n))
-                              run_simulation_sbm(eps = eps,ntimes = 500,n=ns[2],nMC = 500)
-                            }
-stopCluster(cl)
-save(results_sbm_500,file = "sbm_results_10-21_500.Rdata")
-print(paste("finished n =",ns[2]))
-cl <- makeCluster(cores[1]-1) #not to overload your computer
-registerDoParallel(cl,outfile = "sbm_sim_700.txt")
-
-results_sbm_700 <- foreach(eps=epsilons,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
-                            ,.noexport = "generateAdjacencyMatrix" )  %dopar% {
-                              source("./balanced_sbm/sbm_hyp_test.R")
-                              #print(paste("eps = ",eps,", n = ",n))
-                              run_simulation_sbm(eps = eps,ntimes = 500,n=ns[3],nMC = 500)
-                            }
-
-
-save(results_sbm_700,file = "sbm_results_10-21_700.Rdata")
-stopCluster(cl)
-results_sbm <- list(results_sbm_300,results_sbm_500,results_sbm_700)
-save(results_sbm,file="sbm_results_10-21.Rdata")
-print("finished SBM simulations.")#, starting DCSBM simulations")
-
+save(results,file = "sbm_results_10-25.Rdata")
+print("finished.")#,ns[1]))
+# cl <- makeCluster(cores[1]-1) #not to overload your computer
+# registerDoParallel(cl)
+# results_sbm_500 <- foreach(eps=epsilons,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
+#                             ,.noexport = "generateAdjacencyMatrix" )  %dopar% {
+#                               source("./balanced_sbm/sbm_hyp_test.R")
+#                               #print(paste("eps = ",eps,", n = ",n))
+#                               run_simulation_sbm(eps = eps,ntimes = 100,n=ns[2],nMC = 500)
+#                             }
+# stopCluster(cl)
+# save(results_sbm_500,file = "sbm_results_10-21_500.Rdata")
+# print(paste("finished n =",ns[2]))
+# cl <- makeCluster(cores[1]-1) #not to overload your computer
+# registerDoParallel(cl)
+# 
+# results_sbm_700 <- foreach(eps=epsilons,.packages=c('nonparGraphTesting','irlba','igraph','Rcpp','Matrix')
+#                             ,.noexport = "generateAdjacencyMatrix" )  %dopar% {
+#                               source("./balanced_sbm/sbm_hyp_test.R")
+#                               #print(paste("eps = ",eps,", n = ",n))
+#                               run_simulation_sbm(eps = eps,ntimes = 100,n=ns[3],nMC = 500)
+#                             }
+# 
+# 
+# save(results_sbm_700,file = "sbm_results_10-21_700.Rdata")
+# stopCluster(cl)
+# results_sbm <- list(results_sbm_300,results_sbm_500,results_sbm_700)
+# save(results_sbm,file="sbm_results_10-21.Rdata")
+# print("finished SBM simulations.")#, starting DCSBM simulations")
+# 
